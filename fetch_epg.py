1de2d347e8909d07964a18764a66b149b2a5a9bc
01dangerpubg01-cmyk <01dangerpubg01@gmail.com>
2026-06-17 14:36:53 +0530

    Update fetch_epg.py

--- fetch_epg.py
+++ fetch_epg.py
@@ -1,29 +1,39 @@
 import os
-import requests
-import gzip
 import io
+import gzip
+import requests
 
 url = os.environ["EPG_SOURCE_URL"]
 
 headers = {
-    "User-Agent": "Mozilla/5.0"
+"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/125.0 Safari/537.36",
+"Accept": "*/*"
 }
 
-r = requests.get(url, headers=headers, timeout=120)
+r = requests.get(
+url,
+headers=headers,
+timeout=120,
+allow_redirects=True
+)
+
+print("Status:", r.status_code)
+
 r.raise_for_status()
 
 data = r.content
 
-# gzip auto extract
+# Auto unzip if source is gzipped
+
 if data[:2] == b"\x1f\x8b":
-    data = gzip.GzipFile(fileobj=io.BytesIO(data)).read()
+data = gzip.GzipFile(fileobj=io.BytesIO(data)).read()
 
 text = data.decode("utf-8", errors="ignore")
 
-if "<tv" not in text or "<channel" not in text:
-    raise Exception("Invalid XMLTV file")
+if "<tv" not in text:
+raise Exception("Invalid XMLTV file")
 
 with open("epg.xml", "w", encoding="utf-8") as f:
-    f.write(text)
+f.write(text)
 
-print("Valid XMLTV saved")
+print("EPG Updated")
